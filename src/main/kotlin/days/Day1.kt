30d291a8a5e378d474bf3c53f3426a0b023ba8ec
Karol Teske <karol.teske@allegro.pl>
2020-12-01 06:42:10 +0100

    Day 1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,39 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): String {
-        return inputAsListOfString.take(3)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val results = hashSetOf<Int>()
+        val input = inputAsListOfInt
+
+        input.forEach {
+            if (results.contains(target - it)) {
+                return ((target - it) * it).toString()
+            }
+            results.add(it)
+        }
+
+        throw IllegalStateException()
     }
 
     override fun partTwo(): String {
-        return inputAsString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.toUpperCase() }
-            .last()
+        val input = inputAsListOfInt
+        val results = mutableMapOf<Int, Pair<Int, Int>>()
+
+        for (i in 0..input.size - 2) {
+            for (j in i until input.size) {
+                results[input[i] + input[j]] = Pair(input[i], input[j])
+            }
+        }
+
+        for (i in 0..input.size - 3) {
+            if (results.containsKey(target - input[i])) {
+                val pair = results.getOrDefault(target - input[i], Pair(0, 0))
+                return (pair.first * pair.second * input[i]).toString()
+            }
+        }
+        throw IllegalStateException()
+    }
+
+    companion object {
+        const val target = 2020
     }
 }
--- src/test/groovy/Day1Spec.groovy
+++ src/test/groovy/Day1Spec.groovy
@@ -5,12 +5,20 @@ class Day1Spec extends Specification {
 
     def day1 = new Day1()
 
-    def "should solve simple case"(){
+    def "should solve part one"(){
         when:
         def result = day1.partOne()
 
         then:
-        result == "KOTLIN IS GREAT"
+        result == "838624"
 
+    }
+
+    def "should solve part two"(){
+        when:
+        def result = day1.partTwo()
+
+        then:
+        result == "52764180"
     }
 }
